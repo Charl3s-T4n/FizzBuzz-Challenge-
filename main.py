5bcf0292e8d13bb4d50db2d406dcf8b5e5b6a859
Charl3s-T4n <104561880+Charl3s-T4n@users.noreply.github.com>
2022-04-29 00:58:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Write your code below this row 👇
 
 for number in range(1,101):
-  if number % 15 == 0:     #Have to put this condition first,
+  if number % 15 == 0:     #Have to put this condition first: E.g: If 3 is placed first instead of 15, number 15 will output Fizz instead of FizzBuzz due to if-elif condition
     print("FizzBuzz")
   elif number % 3 == 0:
     print("Fizz")
